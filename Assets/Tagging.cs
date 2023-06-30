868ea5e75d55e1ce0b8b82cd0f298708beffd60c
Jiahe <jiahejiahelyu@126.com>
2023-06-30 10:20:05 -0700

    first commit

---
+++ Assets/Tagging.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tagging : MonoBehaviour
+{
+    public Transform target; // Reference to the player's transform
+    public float moveSpeed = 5f; // Speed at which the object chases the player
+
+    private void Update()
+    {
+        // Check if the target (player) is available
+        if (target == null)
+        {
+            Debug.LogWarning("Target (player) is not assigned!");
+            return;
+        }
+
+        // Calculate the direction towards the player
+        Vector3 direction = target.position - transform.position;
+
+        // Normalize the direction vector
+        direction.Normalize();
+
+        // Calculate the object's movement for this frame
+        Vector3 movement = direction * moveSpeed * Time.deltaTime;
+
+        // Move the object towards the player
+        transform.position += movement;
+    }
+}
